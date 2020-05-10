0dc2aa7ea96c02ed7c727d962f53952cccbc1bc1
Chris Rodriguez <52025505+chrisleerod@users.noreply.github.com>
2020-05-10 01:02:11 -0500

    Add files via upload

--- JS/cursor.js
+++ JS/cursor.js
@@ -1,13 +1,43 @@
 let mouseCursor = document.querySelector('.cursor');
 let link = document.querySelectorAll('a');
 let nav = document.querySelectorAll('.hamburger');
+var $ = document.querySelector.bind(document);
+var $on = document.addEventListener.bind(document);
+var followMouse = function() {
+  key = requestAnimationFrame(followMouse);
 
-window.addEventListener('mousemove', cursor);
+  if (!x || !y) {
+    x = xmouse;
+    y = ymouse;
+  } else {
+    dx = (xmouse - x) * 0.125;
+    dy = (ymouse - y) * 0.125;
+    if (Math.abs(dx) + Math.abs(dy) < 0.1) {
+      x = xmouse;
+      y = ymouse;
+    } else {
+      x += dx;
+      y += dy;
+    }
+  }
+  cursor.style.left = x + 'px';
+  cursor.style.top = $(".wrap").scrollTop + y + 'px';
+};
 
-function cursor(e) {
-    mouseCursor.style.top = e.pageY + 'px';
-    mouseCursor.style.left = e.pageX + 'px';
-}
+var xmouse, ymouse;
+var cursor = $('.cursor');
+var x = void 0,
+  y = void 0,
+  dx = void 0,
+  dy = void 0,
+  tx = 0,
+  ty = 0,
+  key = -1;
+
+$on('mousemove', function(e) {
+  xmouse = e.clientX || e.pageX;
+  ymouse = e.clientY || e.pageY;
+});
 
 link.forEach(link => {
     link.addEventListener('mouseover', () => {
