5052c529aff136b40655ba16dde2d5e551736ad7
Chris Rodriguez <52025505+chrisleerod@users.noreply.github.com>
2020-05-30 01:04:45 -0500

    Add files via upload

--- JS/index-app.js
+++ JS/index-app.js
@@ -8,6 +8,6 @@ var tl = gsap.timeline();
 tl
     .from(".spanAnim div", { duration: 0.75, y: 80, skewY: 5, stagger: 0.075, ease: "circ.out" }, 0.75)
     .from(".border", { duration: 1.5, scaleX: 0, transformOrigin: "left", ease: "circ.out" }, 2.1)
-    .from(".arrow-text", { duration: 1, y: 51, skewY: 15 }, 2)
-    .from(".arrow", { duration: 1, y: -51 }, 2)
-    .from(".projects", { duration: 1, y: 51 }, 2.5)
+    .from(".arrow-text", { duration: 1, y: 51, skewY: 15, ease: "circ.out" }, 2)
+    .from(".arrow", { duration: 1, y: -51, ease: "circ.out" }, 2)
+    .from(".projects", { duration: 1, y: 101, skewY: 15, ease: "circ.out" }, 2.5)
